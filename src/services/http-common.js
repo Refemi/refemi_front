8a09fa1264712a6e7b910ffc606bb0c772de4572
Laura Illouz <73298340+Lauraillouz@users.noreply.github.com>
2021-12-30 12:05:05 +0100

    Revert "07 html for seo"

--- src/services/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: process.env.REACT_APP_API,
-  headers: {
-    "Content-type": "application/json",
-  },
-});
--- src/utils/roles.js
+++
@@ -1,7 +0,0 @@
-const roles = {
-    CONTRIBUTOR: 1,
-    MODERATOR: 2,
-    ADMIN: 3
-}
-
-export default roles
