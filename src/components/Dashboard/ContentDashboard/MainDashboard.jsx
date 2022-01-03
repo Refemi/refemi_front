a2aa7d2dc1b59d52dd2f025223c59c0a69d7bbcb
gaev <gaev@cenne.xyz>
2022-01-03 22:19:13 +0100

    update: display user contributions

--- src/components/Dashboard/ContentDashboard/MainDashboard.jsx
+++ src/components/Dashboard/ContentDashboard/MainDashboard.jsx
@@ -25,18 +25,50 @@ const renderContributorDashboard = (contributions) => {
   );
 };
 
+const renderAdminDashboard = (contributions) => {
+  const rContributions = contributions.reduce((filter, contribution) => {
+    if (contribution.status) {
+      filter.validated.push(contribution);
+    } else {
+      filter.pending.push(contribution);
+    }
+    return filter;
+
+  }, {
+    validated: [],
+    pending: []
+  });
+
+  return (
+    <>
+      {rContributions.pending.length > 0 && (
+        <ContributionsDashboard
+          title="Contributions en attente"
+          contributions={rContributions.pending}
+        />
+      )}
+      {rContributions.validated.length > 0 && (
+        <ContributionsDashboard
+          title="Contributions validées"
+          contributions={rContributions.validated}
+        />
+      )}
+    </>
+  );
+};
+
 // COMPONENT
 export default function MainDashboard({ contributions }) {
   const { userCredentials } = useContext(UserCredentials);
 
   return (
     <article className="dashboard-content borders mt-6">
-      {contributions.validated === 0 && contributions.pending === 0 && (
+      {contributions.length === 0 && (
         <p>Aucune contribution validée</p>
       )}
       {/* TODO: render admin dashboard */}
-      {userCredentials.role === roles.CONTRIBUTOR
-        ? renderContributorDashboard(contributions)
+      {userCredentials.role === roles.ADMIN
+        ? renderAdminDashboard(contributions)
         : null}
     </article>
   );
