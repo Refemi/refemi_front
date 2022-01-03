992721dcfc6b0a465619b4550e5d438daf3497b6
laura.illouz <laura.illouz@raja.com>
2022-01-03 15:03:46 +0100

    search OK

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -18,22 +18,24 @@ export default function Search() {
   // TODO: watch out the cross in the input. It goes everywhere. It needs to be in absolute position relative to a div that has in it just the input and the cross.
 
   return (
-    <div className="search margin-top20 flex is-flex-direction-column is-align-items-center">
-      <p className="margin-top">
+    <main className="search is-flex is-flex-direction-column is-align-items-center">
+      <h2 className="mt-6 has-text-centered has-text-weight-bold">
         Tapez votre mot-clé pour trouver la liste des références associées
-      </p>
+      </h2>
 
-      <input
-        type="text"
-        placeholder="Recherche"
-        value={search}
-        onChange={inputSearch}
-        className="borders form-input width80 m-6"
-      />
+      <fieldset className="is-relative is-flex is-justify-content-center">
+        <input
+          type="text"
+          placeholder="Recherche"
+          value={search}
+          onChange={inputSearch}
+          className="borders form-input search-input my-6"
+        />
 
-      <IoIosClose className="clearBtn" onClick={clearInput} />
+        <IoIosClose className="clearBtn" onClick={clearInput} />
+      </fieldset>
 
       {search === "" ? null : <SearchResult answer={search} />}
-    </div>
+    </main>
   );
 }
