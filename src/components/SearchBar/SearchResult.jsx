b8584fedfd1c3f744cb3439a9c610e9aac0a04df
laura.illouz <laura.illouz@raja.com>
2022-01-04 10:50:33 +0100

    updated searchResults to match ListReferences for now

--- src/components/SearchBar/SearchResult.jsx
+++ src/components/SearchBar/SearchResult.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
 import PropTypes from "prop-types";
 import http from "../../services/http-common";
+import { v4 as uuidv4 } from "uuid";
 
 // Get what user types in search input and format it to be processed by backend
 const getSearchInfo = async (answer) => {
@@ -31,25 +32,37 @@ export default function SearchResult({ answer = "" }) {
     const fetchData = async () => {
       setSearchInfo(await getSearchInfo(answer));
     };
-
     fetchData();
   }, [answer]);
 
+  useEffect(() => {
+    console.log(searchInfo);
+  }, [searchInfo]);
+
   return (
     <section className="dataResult">
-      {Array.isArray(searchInfo) &&
-        searchInfo.map((item) => (
-          <article
-            key={item.id}
-            id={item.id}
-            className="description-center has-text-center borders is-flex is-justify-content-space-between line description"
-            onClick={() => history.push(`/references/${item.id}`)}
-          >
-            <div className="reflist-div">{item.reference_name}</div>
-            <div className="reflist-div">{item.reference_country_name}</div>
-            <div className="is-align-self-flex-end reflist-div" />
-          </article>
-        ))}
+      {Array.isArray(searchInfo) && (
+        <div>
+          <h2 className="mb-6 darkblue-text has-text-weight-bold">
+            {searchInfo.length} résultats trouvés pour votre recherche "{answer}
+            " :
+          </h2>
+          {searchInfo.map((item) => (
+            <article
+              key={uuidv4()}
+              id={item.id}
+              className="description-center-reference has-text-center borders is-flex is-justify-content-space-between line m-3"
+              onClick={() => history.push(`/references/${item.id}`)}
+            >
+              <h3 className="reflist-div">{item.reference_name}</h3>
+              <p className="reflist-div is-hidden-mobile has-text-centered">
+                {item.reference_country_name}
+              </p>
+              <div className="is-align-self-flex-end reflist-div" />
+            </article>
+          ))}
+        </div>
+      )}
     </section>
   );
 }
