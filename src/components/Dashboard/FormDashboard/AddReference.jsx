33d6642d37fa309bbf22c16145f23b24cee3c200
gaev <gaev@cenne.xyz>
2022-01-22 04:48:30 +0100

    fix: form reference

--- src/components/Dashboard/FormDashboard/AddReference.jsx
+++ src/components/Dashboard/FormDashboard/AddReference.jsx
@@ -1,12 +1,14 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 // Import components
 import FormReference from "./FormReference";
 import SelectReference from "./SelectReference";
 
+// Import contexts
 import { DashboardContext } from "../../../views/Dashboard";
 
-// Import contexts
+// Create contexts
+export const FormContext = createContext()
 /**
  * Display main form to add a reference
  * @returns {JSX.Element}
@@ -15,6 +17,7 @@ export default function AddReference() {
   const [currentForm, setCurrentForm] = useState("");
   const [showForm, setShowForm] = useState(false);
   const [categories, setCategories] = useState([]);
+  const [currentSection, setCurrentSection] = useState("");
   const { setShowNewRef } = useContext(DashboardContext);
 
   const handleChange = (e) => setCurrentForm(e.nativeEvent.target.value);
@@ -40,23 +43,25 @@ export default function AddReference() {
 
       <p className="m-3 refemi">Soumettre une nouvelle contribution</p>
 
-      {showForm ? (
-        <article className="is-flex is-flex-direction-column form-content">
-          <button
-            onClick={() => setShowForm(false)}
-            className="pointer send-btn darkblue-bg has-text-white is-align-self-flex-end"
-          >
-            Retour aux rubriques
-          </button>
-          <FormReference category={currentForm} categories={categories} />
-        </article>
-      ) : (
-        <SelectReference
-          handleChangeForm={handleChange}
-          setCategories={setCategories}
-          categories={categories}
-        />
-      )}
+      {showForm
+        ? (
+          <article className="is-flex is-flex-direction-column form-content">
+            <button
+              onClick={() => setShowForm(false)}
+              className="pointer send-btn darkblue-bg has-text-white is-align-self-flex-end"
+            >
+              Retour aux rubriques
+            </button>
+            <FormReference category={currentForm} categories={categories.filter(category => category.section_id === currentSection)} />
+          </article>
+        ) : (
+          <SelectReference
+            currentSection={currentSection}
+            setCurrentSection={setCurrentSection}
+            handleChangeForm={handleChange}
+          />
+        )
+      }
     </section>
   );
 }
