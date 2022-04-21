61225ac380f870c9a5f04390c9f5391b74264961
Laura Illouz <lauraillouz.contact@gmail.com>
2022-04-21 17:45:33 +0200

    make header disappear on scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,36 @@
-import React from "react";
-import { useHistory } from "react-router";
-import { VscSearch } from "react-icons/vsc";
+import React from 'react';
+import { useHistory } from 'react-router';
+import { VscSearch } from 'react-icons/vsc';
+import { useScrollDirection } from '../../utils/useScrollDirection';
 
 // Component
-import Navbar from "./Navbar";
+import Navbar from './Navbar';
 
 // COMPONENT
 export default function Header() {
   const history = useHistory();
+  const scrollDirection = useScrollDirection();
 
   return (
-    <header className="refemi-navbar is-flex is-justify-content-space-around">
-      <picture className="logo pointer">
+    <header
+      className={`${
+        scrollDirection === 'down'
+          ? 'refemi-navbar refemi-navbar_down'
+          : 'refemi-navbar'
+      } is-flex is-justify-content-space-around`}
+    >
+      <picture className='logo pointer'>
         <span
-          className="logo-square"
-          onClick={() => history.push("/categories")}
+          className='logo-square'
+          onClick={() => history.push('/categories')}
         />
-        <span className="logo-square" onClick={() => history.push("/themes")} />
+        <span className='logo-square' onClick={() => history.push('/themes')} />
         <span
-          className="logo-square"
-          onClick={() => history.push("/dashboard")}
+          className='logo-square'
+          onClick={() => history.push('/dashboard')}
         />
         <h1>
-          <a href="/" className="refemi logo-square_link">
+          <a href='/' className='refemi logo-square_link'>
             refemi
           </a>
         </h1>
@@ -30,9 +38,9 @@ export default function Header() {
 
       <Navbar />
       <VscSearch
-        className="icon-navbar is-align-self-center pointer"
+        className='icon-navbar is-align-self-center pointer'
         size={20}
-        onClick={() => history.push("/search")}
+        onClick={() => history.push('/search')}
       />
     </header>
   );
