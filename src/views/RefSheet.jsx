f737108b04420a5fcfa375acd62a8f7d124bb593
Laura Illouz <lauraillouz.contact@gmail.com>
2023-04-07 21:39:34 +0200

    just display reference content for now. Stille needs a lot of work it terms of data formatting

--- src/views/RefSheet.jsx
+++ src/views/RefSheet.jsx
@@ -60,10 +60,86 @@ export default function RefSheet() {
 
           <RefSheetHeader reference={reference} key={uuidv4()} />
 
-          <article
-            className="reference-content-details white-bg borders  m-6"
-            dangerouslySetInnerHTML={{ __html: reference.content }}
-          ></article>
+          <article className="reference-content-details white-bg borders  m-6">
+            {reference.aboutauthor ? (
+              <div>
+                <h3>À propos de l'auteur.ice</h3>
+                <p>{reference.aboutauthor}</p>
+              </div>
+            ) : null}
+            {reference.aboutreference ? (
+              <div>
+                <h3>À propos de la référence</h3>
+                <p>{reference.aboutreference}</p>
+              </div>
+            ) : null}
+            {reference.actors ? (
+              <div>
+                <h3>Acteurs</h3>
+                <p>{reference.actors}</p>
+              </div>
+            ) : null}
+            {reference.analysis ? (
+              <div>
+                <h3>Analyse</h3>
+                <p>{reference.analysis}</p>
+              </div>
+            ) : null}
+            {reference.backcover ? (
+              <div className="mt-5">
+                <h3>4ème de couverture</h3>
+                <p>{reference.backcover}</p>
+              </div>
+            ) : null}
+            {reference.bookstructure ? (
+              <div className="mt-5">
+                <h3>Structure</h3>
+                <p>{reference.bookstructure}</p>
+              </div>
+            ) : null}
+            {reference.context ? (
+              <div className="mt-5">
+                <h3>Contexte</h3>
+                <p>{reference.context}</p>
+              </div>
+            ) : null}
+            {reference.episodes ? (
+              <div className="mt-5">
+                <h3>Épisodes</h3>
+                <p>{reference.episodes}</p>
+              </div>
+            ) : null}
+            {reference.extractsandquotes ? (
+              <div className="mt-5">
+                <h3>Extraits et citations</h3>
+                <p>{reference.extractsandquotes}</p>
+              </div>
+            ) : null}
+            {reference.links ? (
+              <div className="mt-5">
+                <h3>Liens</h3>
+                <p>{reference.links}</p>
+              </div>
+            ) : null}
+            {reference.sources ? (
+              <div className="mt-5">
+                <h3>Sources</h3>
+                <p>{reference.sources}</p>
+              </div>
+            ) : null}
+            {reference.synopsis ? (
+              <div className="mt-5">
+                <h3>Synopsis</h3>
+                <p>{reference.synopsis}</p>
+              </div>
+            ) : null}
+            {reference.togofurther ? (
+              <div className="mt-5">
+                <h3>Pour aller plus loin</h3>
+                <p>{reference.togofurther}</p>
+              </div>
+            ) : null}
+          </article>
           <BlueButton spacing="m-3" path="back" label={frenchKeys.back} />
 
           {/*to validated  the reference*/}
